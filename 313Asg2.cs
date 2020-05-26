9fa25b144327fa525dba99ed4e3da34af328b01e
Carl Velasco <carlvelasco96@gmail.com>
2020-05-26 15:37:46 +1200

    Update 313Asg2.cs

--- 313Asg2.cs
+++ 313Asg2.cs
@@ -1,43 +1,41 @@
 class FiniteStateTable
 {
-	int[,] FST = new int[3,2]; //3 rows 2 cols
-	
-	struct cell_FST //groups variables of a cell (indexOfNextState and action)
-	{
-		public int indexOfNextState;
-		public string action;
-	}
+  int[,] FST_X = new int[3, 3]; // 3 rows 3 cols
+  int[,] FST_Y = new int[3, 2]; // 3 Events by 2 States
 
-	//set indexOfNextState of cell
-	public void SetNextState(int stateIndex, int newIndexOfNextState)
-	{
-		FST(state, 2).indexOfNextState = newIndexOfNextState;
-	}
+  struct cell_FST //groups variables of a cell (indexOfNextState and action)
+  {
+    public int indexOfNextState;
+    public string action;
+  }
+
+  //set indexOfNextState of cell
+  public void SetNextState(int stateIndex, int newIndexOfNextState)
+  {
+    FST_X(state, 2).indexOfNextState = newIndexOfNextState;
+  }
+
+  //set action of cell
+  public void SetActions(int stateIndex, string newAction)
+  {
+    FST_X(state, 2).action = newAction;
+  }
+
+  //return indexOfNextState of cell
+  public string GetNextState(int stateIndex)
+  {
+    return FST_X(state, 2).indexOfNextState;
+  }
+
+  //return action of cell
+  public string GetActions(int stateIndex)
+  {
+    return FST_X(state, 1).action;
+  }
+
 	
-	//set action of cell
-	public void SetActions(int stateIndex, string newAction) 
-	{
-		FST(state, 2).action = newAction;
-	}
-	
-	//return indexOfNextState of cell
-	public string GetNextState(int stateIndex) 
-	{
-		return FST(state, 2).indexOfNextState;
-	}
-	
-	//return action of cell
-	public string GetActions(int stateIndex) 
-	{
-		return FST(state, 1).action;
-	}
-	
-	
-	static void Main()
-	{
-		
-		
-		
-		
-	}
+  static void Main()
+  {
+
+  }
 }
