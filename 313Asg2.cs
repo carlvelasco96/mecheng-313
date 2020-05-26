3205c0192932201eac4189beba61904f0ed3189f
Apowo-glitch <65378381+Apowo-glitch@users.noreply.github.com>
2020-05-26 19:55:53 +1200

    Update 313Asg2.cs

--- 313Asg2.cs
+++ 313Asg2.cs
@@ -1,77 +1,125 @@
+using System;
+
 class FiniteStateTable
 {
-  int[,] FST_X = new int[3, 3]; // 3 rows 3 cols
-  int[,] FST_Y = new int[3, 2]; // 3 Events by 2 States
+    public const int S0 = 0;
+    public const int S1 = 1;
+    public const int S2 = 2;
+    public const int E0 = 0;
+    public const int E1 = 1;
+    public const int E2 = 2;
 
-  struct cell_FST //groups variables of a cell (indexOfNextState and action)
-  {
-    public int indexOfNextState;
-    public string action;
-  }
+    int state = S0;
 
-  // 
+    struct cell_FST //groups variables of a cell (indexOfNextState and 2 actions)
+    {
+        public int nextState;
+        public void action1;
+        public void action2;
+    }
 
-  //set indexOfNextState of cell
-  public void SetNextState(int stateIndex, int newIndexOfNextState)
-  {
-    FST_X(state, 2).indexOfNextState = newIndexOfNextState;
-  }
+    cell_FST[3, 3] FST_X = {
+    // S0                           S1                          S2
+    { {S1, ActionX, ActionY},       {S0, ActionW, NoAction},    {S0, ActionW, NoAction} },  //E0
+    { {S0, NoAction, NoAction},     {S2, ActionX, ActionZ},     {S2, NoAction, NoAction} }, //E1
+    { {S2, NoAction, NoAction},     {S1, NoAction, NoAction},   {S1, ActionX, ActionY} },   //E2
+    };
 
-  //set action of cell
-  public void SetActions(int stateIndex, string newAction)
-  {
-    FST_X(state, 2).action = newAction;
-  }
+    // 
 
-  //return indexOfNextState of cell
-  public string GetNextState(int stateIndex)
-  {
-    return FST_X(state, 2).indexOfNextState;
-  }
+    //set indexOfNextState of cell
+    public void SetNextState(int state, int eventIndex, int newIndexOfNextState)
+    {
+        FST_X(state, 2).nextState = newIndexOfNextState;
+    }
 
-  //return action of cell
-  public string GetActions(int stateIndex)
-  {
-    return FST_X(state, 1).action;
-  }
+    //set action of cell
+    public void SetActions(int state, int eventIndex, string newAction)
+    {
+        FST_X(state, 2).action = newAction;
+    }
 
-  private void ActionW()
-  {
-    Console.Write("Action W");
-  }
+    //return indexOfNextState of cell
+    public string GetNextState(int stateIndex)
+    {
+        return FST_X(state, 2).indexOfNextState;
+    }
 
-  private void ActionX()
-  {
-    Console.Write("Action X");
-  }
+    //return action of cell
+    public string GetActions(int stateIndex)
+    {
+        return FST_X(state, 1).action;
+    }
 
-  private void ActionY()
-  {
-    Console.Write("Action Y");
-  }
+    private void ActionW()
+    {
+        Console.Write("Action W");
+    }
 
-  private void ActionZ()
-  {
-    Console.Write("Action Z");
-  }
+    private void ActionX()
+    {
+        Console.Write("Action X");
+    }
 
-  private void ActionJ()
-  {
-    Console.Write("Action J");
-  }
+    private void ActionY()
+    {
+        Console.Write("Action Y");
+    }
 
-  private void ActionK()
-  {
-    Console.Write("Action K");
-  }
+    private void ActionZ()
+    {
+        Console.Write("Action Z");
+    }
 
-  private void ActionL()
-  {
-    Console.Write("Action L");
-  }
+    private void ActionJ()
+    {
+        Console.Write("Action J");
+    }
 
-  static void Main()
-  {
+    private void ActionK()
+    {
+        Console.Write("Action K");
+    }
 
-  }
+    private void ActionL()
+    {
+        Console.Write("Action L");
+    }
+
+    private void NoAction()
+    {
+        //do nothing
+    }
+
+    private void new_state(int newState)
+    {
+        state = newState;
+        Console.WriteLine("Now in State " + state);
+    }
+
+    void Main()
+    {
+        char keyEntered = '0';
+        int keyEvent;
+
+        while (1)
+        {
+            keyEntered = Console.ReadKey();
+            keyEvent = -1;
+            if (keyEntered == ("a")) keyEvent = E0;
+            if (keyEntered == ("b")) keyEvent = E1;
+            if (keyEntered == ("c")) keyEvent = E2;
+            if (keyEvent > 0)
+            {
+                FST_X[keyEntered][state].action1();
+                FST_X[keyEntered][state].action2();
+
+                if (state != FST_X[keyEntered][state].nextState)
+                {
+                    new_state(FST_X[keyEntered][state].nextState);
+                }
+            }
+        }
+    }
+
 }
